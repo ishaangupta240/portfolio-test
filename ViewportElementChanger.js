3d02ced5d22cedcb6d857ad9b20113927fe907b3
Ishaan <67232390+ishaangupta240@users.noreply.github.com>
2024-02-16 02:32:49 +0530

    N/A

--- ViewportElementChanger.js
+++ ViewportElementChanger.js
@@ -15,7 +15,7 @@ var introTag = document.getElementById("intro");
 var minHeight = introTag.clientHeight;
 
 function introTagHeight(){
-    introTag.style.minHeight = minHeight + 1 + "px";
+    introTag.style.minHeight = maxHeight + 1 + "px";
 }
 introTagHeight();
 
